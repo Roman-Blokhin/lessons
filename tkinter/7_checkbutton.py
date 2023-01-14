64ae21f43c146ebbc1577017b48e663311c959dc
Roman <11-roman@mail.ru>
2023-01-14 15:47:03 +0300

    add def 'select _all' with method 'select'

--- tkinter/7_checkbutton.py
+++ tkinter/7_checkbutton.py
@@ -2,14 +2,26 @@
 
 from tkinter import *
 
+# Прописываем функцию для перебора каждого элемента списка, чтобы активировать галочки
+def select_all ():
+    for i in [year, marketing, subscribe]:
+        i.select () # активирует наши checkbuttons
+
 root = Tk ()
 root.title ('')
 root.geometry ('350x350+200+200')
 root.config (bg = '')
 
-Checkbutton (text = 'Вам исполнилось 18 лет?').pack ()
-Checkbutton (text = 'Хотите получать рекламу?').pack ()
-Checkbutton (text = 'Готовы подписаться??', indicatoron = False).pack ()
+year = Checkbutton (text = 'Вам исполнилось 18 лет?')
+year.pack ()
+
+marketing = Checkbutton (text = 'Хотите получать рекламу?')
+marketing.pack ()
+
+subscribe = Checkbutton (text = 'Готовы подписаться??', indicatoron = False)
 # indicatoron = False - можно написать : indicatoron = 0, но это будет не совсем корректно
+subscribe.pack ()
+
+Button (text = 'Выбрать все галочки', command = select_all).pack ()
 
 root.mainloop ()
