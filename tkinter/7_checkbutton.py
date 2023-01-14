e5b9a080fdaafc74ecff42ca08e4f72cdc6c3871
Roman <11-roman@mail.ru>
2023-01-14 13:17:12 +0300

    change view our checkbutton with indicatoron

--- tkinter/7_checkbutton.py
+++ tkinter/7_checkbutton.py
@@ -9,6 +9,7 @@ root.config (bg = '')
 
 Checkbutton (text = 'Вам исполнилось 18 лет?').pack ()
 Checkbutton (text = 'Хотите получать рекламу?').pack ()
-Checkbutton (text = 'Готовы подписаться??').pack ()
+Checkbutton (text = 'Готовы подписаться??', indicatoron = False).pack ()
+# indicatoron = False - можно написать : indicatoron = 0, но это будет не совсем корректно
 
 root.mainloop ()
