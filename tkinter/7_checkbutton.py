c88e13b7db996a1a766f21cc11f8dd8578e9ca4c
Roman <11-roman@mail.ru>
2023-01-14 13:09:59 +0300

    add checkbuttons

--- tkinter/7_checkbutton.py
+++ tkinter/7_checkbutton.py
@@ -7,4 +7,8 @@ root.title ('')
 root.geometry ('350x350+200+200')
 root.config (bg = '')
 
+Checkbutton (text = 'Вам исполнилось 18 лет?').pack ()
+Checkbutton (text = 'Хотите получать рекламу?').pack ()
+Checkbutton (text = 'Готовы подписаться??').pack ()
+
 root.mainloop ()
