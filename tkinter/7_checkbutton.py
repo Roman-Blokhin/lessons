d0ca97067f6232b68b655d07f5fb9121f2f490e0
Roman <11-roman@mail.ru>
2023-01-14 21:57:52 +0300

    add variables and wright attributs to our checkbutton

--- tkinter/7_checkbutton.py
+++ tkinter/7_checkbutton.py
@@ -2,38 +2,61 @@
 
 from tkinter import *
 
+
+# ---------------------------------------------------- 2. ФУНКЦИИ
+
 # Прописываем функцию для перебора каждого элемента списка, чтобы активировать галочки
-def select_all ():
+def select_all():
     for i in [year, marketing, subscribe]:
-        i.select () # активирует наши checkbuttons
+        i.select()  # активирует наши checkbuttons
+
 
 # Прописываем функцию для перебора каждого элемента списка, чтобы ДЕактивировать галочки
-def deselect_all ():
+def deselect_all():
     for i in [year, marketing, subscribe]:
-        i.deselect () # ДЕактивирует наши checkbuttons
+        i.deselect()  # ДЕактивирует наши checkbuttons
+
 
 # Прописываем функцию, чтобы проставить противоположное значение галочек
-def toggle_all ():
+def toggle_all():
     for i in [year, marketing, subscribe]:
-        i.toggle () # ДЕактивирует наши checkbuttons
+        i.toggle()  # ДЕактивирует наши checkbuttons
 
-root = Tk ()
-root.title ('')
-root.geometry ('350x350+200+200')
-root.config (bg = '')
 
-year = Checkbutton (text = 'Вам исполнилось 18 лет?')
-year.pack ()
+# ---------------------------------------------------- НАШЕ ОКНО
 
-marketing = Checkbutton (text = 'Хотите получать рекламу?')
-marketing.pack ()
+root = Tk()
 
-subscribe = Checkbutton (text = 'Готовы подписаться??', indicatoron = False)
+root.title('')
+root.geometry('350x350+200+200')
+root.config(bg='')
+
+# ---------------------------------------------------- 3. ПЕРЕМЕННЫЕ
+
+# создаем переменные, чтобы можно было получать наши данные в консоль
+year_value = StringVar()
+marketing_value = IntVar()
+
+# ---------------------------------------------------- 1. ВИДЖЕТЫ
+
+# 4. связываем переменную с виджетом с помощью variable
+year = Checkbutton(text='Вам исполнилось 18 лет?',
+                   variable=year_value,
+                   onvalue='YES',  # атрибут говорит, что кнопка нажата
+                   offvalue='NO')  # атрибут говорит, что кнопка не нажата
+year.pack()
+
+marketing = Checkbutton(text='Хотите получать рекламу?')
+marketing.pack()
+
+subscribe = Checkbutton(text='Готовы подписаться??', indicatoron=False)
 # indicatoron = False - можно написать : indicatoron = 0, но это будет не совсем корректно
-subscribe.pack ()
+subscribe.pack()
 
-Button (text = 'Выбрать все галочки', command = select_all).pack ()
-Button (text = 'Снять все галочки', command = deselect_all).pack ()
-Button (text = 'Противоположное значение галочек', command = toggle_all).pack ()
+Button(text='Выбрать все галочки', command=select_all).pack()
+Button(text='Снять все галочки', command=deselect_all).pack()
+Button(text='Противоположное значение галочек', command=toggle_all).pack()
 
-root.mainloop ()
+# ---------------------------------------------------- ВАЖНОЕ
+
+root.mainloop()
