5713b9d09532c13db2652f48462ad7ebe78d18f2
Roman <11-roman@mail.ru>
2023-01-16 20:57:21 +0300

    add radiobuttons and label

--- tkinter/radiobutton.py
+++ tkinter/radiobutton.py
@@ -7,4 +7,10 @@ root.title('Radiobutton')
 root.geometry('350x200+200+200')
 root.config(bg='pink')
 
+Label(text='Выберите уровень сложности: ', bg = 'pink', font = ('Arial', 15)).grid(row=0, column=0, columnspan=3)
+
+Radiobutton(text='Easy', bg = 'pink', font = ('Arial', 15)).grid(row=1, column=0)
+Radiobutton(text='Middle', bg = 'pink', font = ('Arial', 15)).grid(row=1, column=1)
+Radiobutton(text='Hard', bg = 'pink', font = ('Arial', 15)).grid(row=1, column=2)
+
 root.mainloop()
