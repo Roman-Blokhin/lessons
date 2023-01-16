27d79877f3bbc55e7ca88eb8177e52ad0d6da2e6
Roman <11-roman@mail.ru>
2023-01-16 21:04:49 +0300

    add variable and values for radiobuttons

--- tkinter/radiobutton.py
+++ tkinter/radiobutton.py
@@ -4,13 +4,18 @@ from tkinter import *
 
 root = Tk()
 root.title('Radiobutton')
-root.geometry('350x200+200+200')
+root.geometry('350x100+200+200')
 root.config(bg='pink')
 
+# 2. создаем числовую переменную и привязываем ее ко всем нашим radiobuttons
+level_var = IntVar ()
+
+# 1. создаем Label и Radiobuttons
 Label(text='Выберите уровень сложности: ', bg = 'pink', font = ('Arial', 15)).grid(row=0, column=0, columnspan=3)
 
-Radiobutton(text='Easy', bg = 'pink', font = ('Arial', 15)).grid(row=1, column=0)
-Radiobutton(text='Middle', bg = 'pink', font = ('Arial', 15)).grid(row=1, column=1)
-Radiobutton(text='Hard', bg = 'pink', font = ('Arial', 15)).grid(row=1, column=2)
+# 3. добавляем атрибут value, чтобы мы могли присвоить значение нашим кнопкам, и они стали нажиматься
+Radiobutton(text='Easy', bg = 'pink', font = ('Arial', 15), variable = level_var, value = 1).grid(row=1, column=0)
+Radiobutton(text='Middle', bg = 'pink', font = ('Arial', 15), variable = level_var, value = 2).grid(row=1, column=1)
+Radiobutton(text='Hard', bg = 'pink', font = ('Arial', 15), variable = level_var, value = 3).grid(row=1, column=2)
 
 root.mainloop()
