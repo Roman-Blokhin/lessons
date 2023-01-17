a4a15521816de2a3d4097091262d25b251d87ecf
Roman <11-roman@mail.ru>
2023-01-17 22:03:11 +0300

    we can get value of combobox

--- tkinter/9_combobox.py
+++ tkinter/9_combobox.py
@@ -1,7 +1,18 @@
 from tkinter import *
 from tkinter import ttk
 
+
+def show_value():
+    print(combo.get())
+    print(num.get())
+    if num.get() == '3':
+        root.config(bg='black')
+    elif combo.get() == 'Воскресенье':
+        but.config(fg='white')
+
+
 weekdays = ('Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье')
+numbers = [1, 2, 3, 4, 5]
 
 root = Tk()
 root.title('Combobox')
@@ -9,7 +20,14 @@ root.geometry('250x250+200+200')
 root.config(bg='grey')
 
 combo = ttk.Combobox(height=5, justify=RIGHT, values=weekdays)
-combo.current (0) # с помощью current по индексу задаем значение, которое будет отображаться по умолчанию
+combo.current(0)  # с помощью current по индексу задаем значение, которое будет отображаться по умолчанию
 combo.pack()
+
+num = ttk.Combobox(height=5, justify=LEFT, values=numbers)
+num.current(4)  # с помощью current по индексу задаем значение, которое будет отображаться по умолчанию
+num.pack()
+
+but = Button(text='show_value', bg='green', command=show_value)
+but.pack()
 
 root.mainloop()
