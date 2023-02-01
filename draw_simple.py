3ce1f950b146c29d30649c8adc3bdfb6c345191d
Roman <11-roman@mail.ru>
2023-02-01 16:37:08 +0300

    10 circles

--- draw_simple.py
+++ draw_simple.py
@@ -3,7 +3,7 @@
 
 import simple_draw as sd
 
-sd.resolution = (500, 500) # 1. устанавливаем размер окна
+sd.resolution = (1100, 500) # 1. устанавливаем размер окна
 point = sd.random_point() # 2. можно задать рандомные координаты точки
 sd.circle (center_position = point, color = sd.COLOR_BLACK, radius = 50, width = 5) # 3. круг с координатами точки
 
@@ -20,9 +20,19 @@ def bubble (point, step):
     radius = 50
     for _ in range (3):
         radius += step
-        sd.circle (center_position = point_2, color = sd.random_color(), radius = radius, width = 6)
-point_2 = sd.get_point (100, 150)
+        sd.circle (center_position = point, color = sd.random_color(), radius = radius, width = 6)
+point = sd.get_point (100, 150)
 bubble (point = point, step = 10)
+
+# делаем 10 пузырьков в ряд
+def bubble (point, step):
+    radius = 30
+    for _ in range (2):
+        radius += step
+        sd.circle (center_position = point, color = sd.random_color(), radius = radius, width = 5)
+for x in range (50, 550, 50): # задаем параметры шага создания наших колец
+    point = sd.get_point(x, 400) # передаем координату шага
+    bubble(point=point, step=5)
 
 
 
