00c7a726efa29d27d4199c3c35aa129f35a606c2
Roman <11-roman@mail.ru>
2023-02-01 16:08:56 +0300

    circle

--- draw_simple.py
+++ draw_simple.py
@@ -3,9 +3,20 @@
 
 import simple_draw as sd
 
-sd.resolution = (1000, 200) # устанавливаем размер окна
-point = sd.random_point() # можно задать рандомное значение точки, можно: sd.random_point()
+sd.resolution = (500, 500) # 1. устанавливаем размер окна
+point = sd.random_point() # 2. можно задать рандомные координаты точки
+sd.circle (center_position = point, color = sd.COLOR_BLACK, radius = 50, width = 5) # 3. круг с координатами точки
 
-sd.circle (center_position = point) # создаем круг с координатами точки
+# делаем функцию, которая вызовет 3 круга с шагом 5 пикселей в радиусе
+point_2 = sd.get_point (200, 300) # фиксированные координаты
+radius = 30
+for _ in range (3):
+    radius +=5
+    # color = sd.random_color() -  дает рандомное значение цвета нашей окружности
+    sd.circle (center_position = point_2, color = sd.random_color(), radius = radius, width = 4)
 
-# sd.pause()
+
+
+
+
+sd.pause()
